20114ab560e17e4d354180859fb7005946a13816
Anne Goncalves <anne.goncalves@metaltoad.com>
2024-03-21 13:31:40 -0300

    mui

--- FrontEnd/src/components/Header/Header.tsx
+++ FrontEnd/src/components/Header/Header.tsx
@@ -1,5 +1,14 @@
 import "./Header.scss";
 
+import { Typography } from "@mui/material";
+
 export const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <img src="logo.svg" alt="AgeVision AI Logo" />
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
+    </div>
+  );
 };
