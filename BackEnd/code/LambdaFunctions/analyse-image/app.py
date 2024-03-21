9c4ece55a93387899fbf77fea22a43abf646758d
andrelara2002 <andrelara2002@gmail.com>
2024-03-21 15:31:38 -0300

    Feat: Adding emotions to response

--- BackEnd/code/LambdaFunctions/analyse-image/app.py
+++ BackEnd/code/LambdaFunctions/analyse-image/app.py
@@ -30,7 +30,11 @@ def lambda_handler(event: Dict[str, str], context):
             "Content-Type": "application/json",
         },
     }
-    response: Dict[str, Union[str, float]] = {"Age": 0}
+    response: Dict[str, Union[str, int, List, Dict]] = {
+        "Age": 0,
+        "Emotions": [],
+        "HigherEmotion": {},
+    }
 
     try:
         # Extract information from event
@@ -68,7 +72,7 @@ def lambda_handler(event: Dict[str, str], context):
         # Analyze image
         analysis = rekognition_client.detect_faces(
             Image={"S3Object": {"Bucket": s3_bucket, "Name": object_key}},
-            Attributes=["AGE_RANGE"],
+            Attributes=["AGE_RANGE", "EMOTIONS"],
         )
 
         if len(analysis["FaceDetails"]) == 0:
@@ -80,10 +84,16 @@ def lambda_handler(event: Dict[str, str], context):
         upper_age: int = face_details["AgeRange"]["High"]
         lower_age: int = face_details["AgeRange"]["Low"]
 
+        emotions = [
+            emotion
+            for emotion in face_details["Emotions"]
+            if emotion["Confidence"] > 40
+        ]
         age: int = int((upper_age + lower_age) / 2)
 
         # Update response
         response["Age"] = age
+        response["Emotions"] = emotions  # type: ignore
 
     except ServerError as e:
         api_response["statusCode"] = 500
